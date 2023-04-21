9f7db55cb34ff9986bbbba2c96521774ee943e4c
Deedee <deedeeowoeye@gmail.com>
2023-04-20 20:45:51 -0700

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,9 +11,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(a);
-printf("Size of a int: %e byte(s)\n", (unsigned long)sizeof(b);
-printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(c);
-printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(d);
-printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(f);
+	
+	printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(a);
+	printf("Size of a int: %e byte(s)\n", (unsigned long)sizeof(b);
+	printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(c);
+	printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(d);
+	printf("Size of a char: %e byte(s)\n", (unsigned long)sizeof(f);
 }
