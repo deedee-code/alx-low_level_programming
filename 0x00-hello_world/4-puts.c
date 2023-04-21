5e00176761883c88cb9b617700ef8808cd07517b
Deedee <deedeeowoeye@gmail.com>
2023-04-20 19:57:11 -0700

    initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing a line using the put function.
+ * Return: 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
