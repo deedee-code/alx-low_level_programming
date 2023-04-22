3c492ddea75c68f5c1c11409d5396d6a6bd5e69a
Deedee <deedeeowoeye@gmail.com>
2023-04-21 19:45:05 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints all the numbers of base 16 in lowercase;
+ * Return: 0;
+ */
+int main(void)'
+{
+	int n;
+	int i;
+
+	for (n = 48; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (i = 97; i <= 102; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
