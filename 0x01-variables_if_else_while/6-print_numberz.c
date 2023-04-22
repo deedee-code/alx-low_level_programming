18cbe9f4c9e352ab5eb93aa86f22b7c558530c1f
Deedee <deedeeowoeye@gmail.com>
2023-04-21 19:19:38 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio>
+/**
+ * main - prints all single digit numbers of base 10 start from 0 with putchar;
+ * Return: 0;
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n + 48);
+	}
+	putchar('\n');
+	return (0);
+}
