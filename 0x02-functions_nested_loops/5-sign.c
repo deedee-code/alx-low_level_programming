a01671a73f083e8d9af28e00da49c2d0f0fa42dd
Deedee <deedeeowoeye@gmail.com>
2023-04-25 17:15:37 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_sign - Determines if the inut number
+ * is greater than zero, is zero, is less than zero
+ *
+ * @n: The input nmuber as an integer
+ * Return: 1 is greater than zero, 0 is zero
+ * -1 is less than 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar(48);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar (45);
+		return (-1)
+	}
+	_putchar('\n')
+}
