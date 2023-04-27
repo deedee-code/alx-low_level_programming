91bcba114994d64cd98eb58bcdfd4dbd86d87c6d
Deedee <deedeeowoeye@gmail.com>
2023-04-26 17:36:16 -0700

    initial commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
  * int _abs - compute the absolute value of an integer
  * @i: input nmuber as an integer
  */
-int _abs(int) 
+int _abs(int i)
 {
 	if (i >= 0)
 	{
@@ -11,6 +11,6 @@ int _abs(int)
 	}
 	else
 	{
-		return (i * -1;
+		return (i * -1);
 	}
 }
