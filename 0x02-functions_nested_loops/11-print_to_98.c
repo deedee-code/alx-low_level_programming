a57132a89cbb223f2f1ca7d0ecdacb70d8b74bc2
Deedee <deedeeowoeye@gmail.com>
2023-04-26 19:54:58 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: input a number
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+	for (n = 0; n >= 98; n++)
+	{
+		_putchar(n);
+		_putchar(',');
+		_putchar('\n');
+	}
+	return (0);
+}
