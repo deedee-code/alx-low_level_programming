089c59b1c50831620d2a83cae4a993b98dfb1d91
Deedee <deedeeowoeye@gmail.com>
2023-04-26 19:36:48 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - adds two integers and returns the result
+ * @i: to be added to j
+ * @j: to be added to i
+ * Return: sum of i and j
+ */
+int add(int i, int j)
+{
+	int c;
+	
+	n = (i + j);
+	return (c);
+}
