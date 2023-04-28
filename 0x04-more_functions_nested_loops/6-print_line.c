15bed6388cf25f7963ba13bebd94c0ca9a35ae9d
Deedee <deedeeowoeye@gmail.com>
2023-04-27 19:40:44 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - a function that draws a straight line in the terminal
+ * @n: the number of times
+ * Return: 0
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
