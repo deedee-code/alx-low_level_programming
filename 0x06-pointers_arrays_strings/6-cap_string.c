2687f09c6ee8d7265b6f02f0ab26fd3880d9327b
Deedee <deedeeowoeye@gmail.com>
2023-05-04 09:22:52 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @n: capitalize string
+ * Return: 0
+ */
+
+char *cap_string(char *n)
+{
+	int i = 0;
+
+	while (n[i])
+	{
+		while (!(n[i] >= 'a') && (n[i] <= 'z'))
+			i++;
+		if (n[i - 1] == ' ' ||
+		n[i - 1] == '\t' ||
+		n[i - 1] == '\n' ||
+		n[i - 1] == ',' ||
+		n[i - 1] == ';' ||
+		n[i - 1] == '.' ||
+		n[i - 1] == '!' ||
+		n[i - 1] == '"' ||
+		n[i - 1] == '?' ||
+		n[i - 1] == '(' ||
+		n[i - 1] == ')' ||
+		n[i - 1] == '{' ||
+		n[i - 1] == '}' ||
+		i == 0)
+			n[i] -= 32;
+		i++;
+	}
+	return (n);
+}
