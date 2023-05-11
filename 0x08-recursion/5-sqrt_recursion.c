0b0f753193a9005bd12eb4d9b6b361a5d44a03c0
Deedee <deedeeowoeye@gmail.com>
2023-05-10 17:53:18 -0700

    initail commit

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,8 +24,8 @@ int _sqrt_recursion(int n)
 int _natural_sqrt_recursion(int n, int i)
 {
 	if (i * i > n)
-		return (0);
+		return (-1);
 	if (i * i == n)
-		return (1);
+		return (n);
 	return (_natural_sqrt_recursion(n, i + 1));
 }
